f1589d4bfb8234a03047f2758f2ee0a924c4a12e
Tomoya Usami <tonchi.systemv@gmail.com>
2016-04-24 16:38:41 +0900

    Implemented generate an Authorization header

---
+++ client.go
@@ -0,0 +1,81 @@
+package hawk
+
+import (
+	"crypto/rand"
+	"encoding/hex"
+	"strconv"
+)
+
+type Client struct {
+	Credential Credential
+	Option     Option
+}
+
+type Credential struct {
+	ID  string
+	Key string
+	Alg Alg
+}
+
+type Option struct {
+	TimeStamp   int64
+	Nonce       string
+	ContentType string
+	Hash        string
+	Ext         string
+	App         string
+	Dlg         string
+}
+
+type Alg int
+
+const (
+	_ Alg = iota
+	SHA256
+	SHA512
+)
+
+func (c *Client) Header(uri string, method string) (string, error) {
+	m := &Mac{
+		AuthHeader,
+		c.Credential,
+		uri,
+		method,
+		c.Option,
+	}
+
+	mac, err := m.String()
+	if err != nil {
+		return "", err
+	}
+
+	nonce, err := Nonce(8)
+	if err != nil {
+		return "", err
+	}
+
+	header := "Authorization: Hawk " +
+		"id=" + c.Credential.ID +
+		", " +
+		"ts=" + strconv.FormatInt(c.Option.TimeStamp, 10) +
+		", " +
+		"nonce=" + nonce
+	if c.Option.Hash != "" {
+		header = header + ", " + "hash=" + c.Option.Hash
+	}
+	if c.Option.Ext != "" {
+		header = header + ", " + "ext=" + c.Option.Ext
+	}
+	header = header + ", " + mac
+
+	return header, nil
+}
+
+func Nonce(n int) (string, error) {
+	bytes := make([]byte, n)
+	_, err := rand.Read(bytes)
+	if err != nil {
+		return "", err
+	}
+	return hex.EncodeToString(bytes), nil
+}
---
+++ crypto.go
@@ -0,0 +1,100 @@
+package hawk
+
+import (
+	"crypto/hmac"
+	"crypto/sha256"
+	"crypto/sha512"
+	"encoding/base64"
+	"hash"
+	"net"
+	"net/url"
+	"strconv"
+	"strings"
+)
+
+const headerVersion = 1
+
+type AuthType int
+
+const (
+	AuthHeader AuthType = iota
+	AuthResponse
+	AuthBewit
+)
+
+type Mac struct {
+	Type       AuthType
+	Credential Credential
+	Uri        string
+	Method     string
+	Option     Option
+}
+
+func (m *Mac) String() (string, error) {
+	digest, err := m.digest()
+	if err != nil {
+		return "", err
+	}
+	return base64.StdEncoding.EncodeToString(digest), nil
+}
+
+func (m *Mac) digest() ([]byte, error) {
+	h := getHash(m.Credential.Alg)
+
+	mac := hmac.New(h, []byte(m.Credential.Key))
+	ns, err := m.nomarized()
+	if err != nil {
+		return nil, err
+	}
+	mac.Write([]byte(ns))
+
+	return mac.Sum(nil), nil
+}
+
+func (m *Mac) nomarized() (string, error) {
+	return nomarized(m.Type, m.Uri, m.Method, m.Option)
+}
+
+func nomarized(authType AuthType, uri string, method string, option Option) (string, error) {
+	u, err := url.Parse(uri)
+	if err != nil {
+		return "", err
+	}
+
+	host := u.Host
+	_, port, _ := net.SplitHostPort(u.Host)
+	if port == "" {
+		switch u.Scheme {
+		case "http":
+			port = "80"
+		case "https":
+			port = "443"
+		}
+	}
+	path := u.Path
+
+	header := "hawk" + "." + strconv.Itoa(headerVersion) + "." + authType.String()
+
+	ns := header + "\n" +
+		strconv.FormatInt(option.TimeStamp, 10) + "\n" +
+		option.Nonce + "\n" +
+		strings.ToUpper(method) + "\n" +
+		strings.ToLower(path) + "\n" +
+		host + "\n" +
+		port + "\n" +
+		option.Hash + "\n" +
+		option.Ext + "\n"
+
+	return ns, nil
+}
+
+func getHash(alg Alg) func() hash.Hash {
+	switch alg {
+	case SHA256:
+		return sha256.New
+	case SHA512:
+		return sha512.New
+	default:
+		return sha256.New
+	}
+}
